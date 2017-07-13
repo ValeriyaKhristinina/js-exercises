a81b6bf1529cf1941a2bd2cf72145a3b76d64325
Valeriya Khristinina <valeriya.khristinina@gmail.com>
2017-07-13 16:24:50 +0300

    Create task get-last-arrays-element.js

---
+++ get-last-arrays-element.js
@@ -0,0 +1,10 @@
+var goods = [1, 5, 8, 9, 14]
+var lastItemIndex = goods.length - 1
+
+if (lastItemIndex !== 0) {
+  var lastItem = goods[lastItemIndex]
+} else {
+  console.log('Array is empty!')
+}
+
+console.log(lastItem)
