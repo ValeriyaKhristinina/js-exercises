a822b7f4b2aa46b7827553579c68d38c86b7305a
Valeriya Khristinina <valeriya.khristinina@gmail.com>
2017-07-13 16:48:44 +0300

    Create task add-new-element-in-array.js

---
+++ add-new-element-in-array.js
@@ -0,0 +1,9 @@
+var goods = ['Apple', 'Milk', 'Biology']
+
+function addNewArraysElement (array, element) {
+  return array.push(element)
+}
+
+addNewArraysElement(goods, 'MacBook')
+
+console.log(goods)
