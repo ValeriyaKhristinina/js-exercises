2de2b8732066bf279458208f2c848223ccc23bbf
Valeriya Khristinina <valeriya.khristinina@gmail.com>
2017-07-17 20:09:05 +0300

    Create task add-class-in-string.js

---
+++ add-class-in-string.js
@@ -0,0 +1,19 @@
+var example = {
+  className: 'open menu'
+}
+
+function addClass (obj, cls) {
+  var arrayClassName = obj.className.split(' ')
+  if (arrayClassName.indexOf(cls) != -1) {
+    return obj
+  }
+  arrayClassName.push(cls)
+  obj.className = arrayClassName.join(' ')
+  return obj
+}
+
+console.log(addClass(example, 'new'))
+console.log(example)
+
+addClass(example, 'open')
+console.log(example)
